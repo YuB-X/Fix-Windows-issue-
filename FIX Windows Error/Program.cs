0373ebbceea3829953f9f6b84f9b6812b9b8730a
YuB - X <125957104+YuB-X@users.noreply.github.com>
2023-02-27 20:29:01 +0200

    Add files via upload

---
+++ FIX Windows Error/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Win32;
+
+// Specify the registry key path
+const string registryKeyPath = @"HKEY_CLASSES_ROOT\exefile\shell\open\command";
+
+// Specify the new value to set
+const string newValue = "\"%1\" %*";
+
+// Open the registry key for writing
+Registry.SetValue(registryKeyPath, "", newValue);
+
+Console.WriteLine("FIXED");
+
+while (true)
+{
+    Console.ReadLine();
+}
